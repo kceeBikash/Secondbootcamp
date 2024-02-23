7b3cf9b892b16de58352eafc6dfffdb5f9ae2366
Bikash Khatri <your@email.com>
2024-02-23 17:27:21 +0545

    msz

---
+++ demoo.js
@@ -0,0 +1,6 @@
+<div>This is a new deeb component.</div>
+<div>This is a new scscs component.</div>
+<div>This is a new component.</div>
+<div>This is a new  scscsc component.</div>
+<div>This is a new component.</div>
+<div>This is a new component.</div>
